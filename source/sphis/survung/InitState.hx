23c45c143fe1e6bb5e97a54bbcfac893f88a0888
Viwu <sinconsistencia@gmail.com>
2025-11-12 19:42:08 -0600

    SCANLINE_TEST define + 256x256

--- source/sphis/survung/InitState.hx
+++ source/sphis/survung/InitState.hx
@@ -1,5 +1,8 @@
 package sphis.survung;
 
+import sphis.survung.debug.ScanlineTest;
+import haxe.macro.Compiler;
+import flixel.FlxG;
 import flixel.FlxState;
 
 class InitState extends FlxState
@@ -7,6 +10,9 @@ class InitState extends FlxState
 	override public function create()
 	{
 		super.create();
+
+		if (Compiler.getDefine('SCANLINE_TEST') == "1")
+			FlxG.switchState(ScanlineTest.new);
 	}
 
 	override public function update(elapsed:Float)
