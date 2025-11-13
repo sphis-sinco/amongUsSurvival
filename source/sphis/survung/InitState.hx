333e77b2a66408ccdfb99225cf0977df65be10c5
Viwu <sinconsistencia@gmail.com>
2025-11-12 19:48:13 -0600

    center window ( AI generated response :( )

--- source/sphis/survung/InitState.hx
+++ source/sphis/survung/InitState.hx
@@ -1,5 +1,8 @@
 package sphis.survung;
 
+import openfl.Lib;
+import openfl.display.Screen;
+import lime.app.Application;
 import sphis.survung.debug.ScanlineTest;
 import haxe.macro.Compiler;
 import flixel.FlxG;
@@ -12,6 +15,31 @@ class InitState extends FlxState
 		super.create();
 
 		FlxG.resizeWindow(256 * 3, 256 * 3);
+
+		var window = Lib.application.window;
+		var currentScreen = Screen.mainScreen; // Or iterate through Screen.screens to find the relevant one
+
+		if (currentScreen != null)
+		{
+			var screenWidth = currentScreen.bounds.width;
+			var screenHeight = currentScreen.bounds.height;
+
+			// Adjust for window scaling if applicable (e.g., high-DPI displays)
+			var windowWidth = window.width * window.scale;
+			var windowHeight = window.height * window.scale;
+
+			var centerX = (screenWidth - windowWidth) / 2;
+			var centerY = (screenHeight - windowHeight) / 2;
+
+			window.x = Math.floor(centerX);
+			window.y = Math.floor(centerY);
+
+			trace('Centered window');
+		}
+		else
+		{
+			trace('Window cannot be centered due to null main screen');
+		}
 
 		if (Compiler.getDefine('SCANLINE_TEST') == "1")
 			FlxG.switchState(ScanlineTest.new);
