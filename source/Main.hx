9f69f6d1b2ed3fbdd23a05b9a48f23e21cdf2d73
Viwu <sinconsistencia@gmail.com>
2025-11-12 19:26:48 -0600

    Inital Commit

---
+++ source/Main.hx
@@ -0,0 +1,14 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+import sphis.survung.InitState;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(0, 0, InitState));
+	}
+}
